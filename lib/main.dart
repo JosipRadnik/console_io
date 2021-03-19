75b2f5d4f301b234715cc7218652f117d5e47e6f
JosipRadnik <soljicjosip1987@gmail.com>
2021-03-19 20:30:44 +0100

    console_io - single quotes in strings

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  print("What is your name?: ");
+  print('What is your name?: ');
   String name = stdin.readLineSync();
-  print("Hello ${name}.");
+  print('Hello ${name}.');
 }
